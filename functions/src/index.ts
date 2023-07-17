045fce5e45f9279a4d8d08e101ef01dc8f5d76bf
jethrocabaluna <jethrocabaluna310@gmail.com>
2023-07-17 23:19:03 +0800

    hotel fact

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,7 +17,15 @@ type EscapeRoom = {
   imageUrl?: string
 }
 
-const defaultPrompt: ChatCompletionRequestMessage[] = [
+type HotelFact = {
+  question: string
+  choices: string[]
+  answerIndex: number
+  trivia: string
+  references: string[]
+}
+
+const defaultEscapeRoomPrompt: ChatCompletionRequestMessage[] = [
   {
     role: 'system',
     content: 'You are a game designer for an escape room web application. You only speak JSON. Do not write normal text.',
@@ -33,14 +41,38 @@ const defaultPrompt: ChatCompletionRequestMessage[] = [
   },
 ]
 
+const defaultHotelFactPrompt: ChatCompletionRequestMessage[] = [
+  {
+    role: 'system',
+    content: `
+      You are an expert about the history of hotels and hoteliers. And also about the modern hotels and booking platforms like airbnb, booking.com, agoda, etc. You also know a lot about countries and their attractions, cuisines and hospitalities. You only speak JSON. Do not write normal text.
+    `,
+  },
+  {
+    role: 'user',
+    content: `
+      Create an object that contains a question, choices, an answerIndex, a trivia, and references.
+      The question is some interesting and fun quiz question.
+      The choices are three options that are texts and one of them is the answer to the question.
+      The answerIndex is the index of the correct answer in the choices.
+      The trivia is elaborating more about the answer. The trivia should be at least three sentences.
+      The references are url links that supports the trivia. Include a link to a wikipedia about the answer in the references, if possible.
+    `,
+  },
+]
+
 let escapeRoomsPromptHistory: ChatCompletionRequestMessage[] = [
-  ...defaultPrompt,
+  ...defaultEscapeRoomPrompt,
+]
+
+let hotelFactPromptHistory: ChatCompletionRequestMessage[] = [
+  ...defaultHotelFactPrompt,
 ]
 
 app.get('/escape-room', async (req, res) => {
   // resets the prompts
   if (escapeRoomsPromptHistory.length > 20) {
-    escapeRoomsPromptHistory = [...defaultPrompt]
+    escapeRoomsPromptHistory = [...defaultEscapeRoomPrompt]
   }
 
   // setups the prompts to make it more likely to provide a different response
@@ -79,6 +111,44 @@ app.get('/escape-room', async (req, res) => {
     return res.json(escapeRoom)
   } catch (err) {
     logger.error('Failed to generate an escape room', err)
+  }
+
+  return res.status(500).send('Something went wrong. Please try again.')
+})
+
+app.get('/hotel-fact', async (req, res) => {
+  // resets the prompts
+  if (hotelFactPromptHistory.length > 50) {
+    hotelFactPromptHistory = [...defaultHotelFactPrompt]
+  }
+
+  // setups the prompts to make it more likely to provide a different response
+  if (hotelFactPromptHistory[hotelFactPromptHistory.length - 1].role === 'assistant') {
+    hotelFactPromptHistory.push({
+      role: 'user',
+      content: 'Give another one.',
+    })
+  }
+
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: hotelFactPromptHistory,
+  })
+
+  try {
+    const rawJson = response.data.choices[0].message?.content
+    const hotelFact = JSON.parse(rawJson ?? '{}') as HotelFact
+    logger.info('hotelFact', hotelFact)
+
+    // setups the prompts to make it more likely to provide a different response
+    hotelFactPromptHistory.push({
+      role: 'assistant',
+      content: rawJson,
+    })
+
+    return res.json(hotelFact)
+  } catch (err) {
+    logger.error('Failed to generate a hotel fact', err)
   }
 
   return res.status(500).send('Something went wrong. Please try again.')
